d108a7c51758ff4734a561d92708f79e62a9a804
Chris Johnson and Connor miller <chris@>
2017-10-17 13:13:40 -0700

    add image toggle for night mode

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,15 @@ function toggleNightMode() {
   var backgroundColor = document.body.style.backgroundColor;
   var clked = true;
 
-
   if (color == "black" && backgroundColor == "white") {
         document.body.style.color="white";
         document.body.style.backgroundColor="black";
         document.body.style.fontFamily="Arial";
-
+        $("#pugpic").attr("src", "img/pug-face-invert.jpg");
     } else {
         document.body.style.color="black";
         document.body.style.backgroundColor="white";
         document.body.style.fontFamily="Times New Roman";
+        $("#pugpic").attr("src", "img/pug-face.jpg");
     }
 }
